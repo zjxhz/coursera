d1793ec32baf66c29ee9a3685848354e8e28fa0c
zjxhz <xuhuanze@qq.com>
2017-01-12 17:41:15 +0800

    added theta_history

--- src/main/resources/testdata/stanford/machinelearning/week2/ex1/gradientDescent.m
+++ src/main/resources/testdata/stanford/machinelearning/week2/ex1/gradientDescent.m
@@ -1,4 +1,4 @@
-function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+function [theta, J_history,theta_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
 %   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
@@ -6,6 +6,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
+theta_history = zeros(2,num_iters);
 
 for iter = 1:num_iters
 
@@ -20,14 +21,12 @@ for iter = 1:num_iters
 
 
     theta = theta - ((X * theta - y)' * X)' * alpha / m;
-
-
-
-    % ============================================================
+    theta_history(1,iter) = theta(1);
+    theta_history(2,iter) = theta(2);
+     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
-    J_history(iter)
 
 end
 
